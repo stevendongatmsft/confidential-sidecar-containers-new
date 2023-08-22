b12f1860d0e4a5405c3c6b5ad336273339cc4e5b
Steven Dong <stevendong@microsoft.com>
2023-08-22 11:11:00 -0400

    rm debug code

--- pkg/attest/maa.go
+++ pkg/attest/maa.go
@@ -144,16 +144,13 @@ func newAttestSNPRequestBody(snpAttestationReport []byte, vcekCertChain []byte, 
 	// As of today we CANNOT pass the policy as it is rego, so not good json and only json
 	// is currently supported byt MAA, not binary.
 	if false && policyBlob != nil {
-		fmt.Println("this was executed")
 		request.InittimeData = attestedData{
 			Data:     base64.URLEncoding.EncodeToString(policyBlob),
 			DataType: "binary", // rego really
 		}
 	}
-	fmt.Println("zanku ", (false && policyBlob != nil))
 	rand.Seed(time.Now().UnixNano())
 	request.Nonce = rand.Uint64()
-	fmt.Printf("sorewa: \n%+v", request)
 
 	return &request, nil
 }
